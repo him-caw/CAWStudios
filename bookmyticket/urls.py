632c626aa92a1e7755187d3e4756d25bd1c9a110
Himanshu Gupta <him20056@gmail.com>
2021-06-25 10:18:42 +0530

    Added Serializers and Views

--- bookmyticket/urls.py
+++ bookmyticket/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome),
+    path('movies/', views.movies),
+    path('cinemas/', views.cinema),
+    path('showtimes/', views.showtimes),
+    path('shows', views.ongoing_shows),
 ]
